ed259b15331aedbd2d81175bcd4f966564f40cff
AnotherOneOfMyGithubAcc <137302621+AnotherOneOfMyGithubAcc@users.noreply.github.com>
2023-06-21 21:17:53 -0400

    Add files via upload

--- project_website/urls.py
+++ project_website/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('about-the-chamber/', views.about_the_chamber),
-    path('executive-board/', views.executive_board),
-    path('events/', views.events),
-    path('event-pictures/', views.event_pictures),
-    path('event-videos/', views.event_videos),
+    # path('about-the-chamber/', views.about_the_chamber),
+    # path('executive-board/', views.executive_board),
+    # path('events/', views.events),
+    # path('event-pictures/', views.event_pictures),
+    # path('event-videos/', views.event_videos),
     # path('', views.),
     # path('', views.),
     # path('', views.),
